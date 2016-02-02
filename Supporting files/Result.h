25b612185ccd3b152da29069a72d889e3cfaf5fc
Mikhail Stepkin <mikhail.s@ramotion.com>
2016-02-02 11:42:13 +0300

    Removing FOUNDATION_EXPORT

--- Supporting files/Result.h
+++ Supporting files/Result.h
@@ -7,10 +7,10 @@
 //
 
 //! Project version number for Result.
-FOUNDATION_EXPORT double ResultVersionNumber;
+extern double ResultVersionNumber;
 
 //! Project version string for Result.
-FOUNDATION_EXPORT const unsigned char ResultVersionString[];
+extern const unsigned char ResultVersionString[];
 
 // In this header, you should import all the public headers of your framework
 // using statements like #import <Result/PublicHeader.h>
