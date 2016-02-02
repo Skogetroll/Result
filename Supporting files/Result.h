5619238be9df1d3dc23716cfee870d2847f9e1f9
Mikhail Stepkin <mikhail.s@ramotion.com>
2016-02-02 11:06:30 +0300

    More scheme updates

--- Supporting files/Result.h
+++ Supporting files/Result.h
@@ -14,6 +14,5 @@ FOUNDATION_EXPORT double ResultVersionNumber;
 //! Project version string for Result.
 FOUNDATION_EXPORT const unsigned char ResultVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Result/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework
+// using statements like #import <Result/PublicHeader.h>
